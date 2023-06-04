a3c3e1f67002120e07f461b3eedf922b4ce87e25
n2ai <trinhh7@gator.uhd.edu>
2023-06-05 00:39:48 +0700

    set up mui

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <p>Note App</p>
+  )
+}
+
+export default App
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import {Button,Typography} from "@mui/material"
+export default function Login(){
+    return(
+        <>
+            <Typography>Welcome to Note App</Typography>
+            <Button variant="outlined">
+                Login with Google
+            </Button>
+        </>
+    )
+}
